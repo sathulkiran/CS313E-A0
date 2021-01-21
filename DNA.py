5420c3e6e1c103ea79026ba14adb2ddbc1e5f26e
“Athul <s.athulkiran@gmail.com>
2021-01-21 14:18:59 -0600

    first commit

---
+++ DNA.py
@@ -0,0 +1,53 @@
+#  File: DNA.py
+
+#  Description:
+
+#  Student Name: Athul Srinivasaraghavan
+
+#  Student UT EID: as84444
+
+#  Partner Name:
+
+#  Partner UT EID:
+
+#  Course Name: CS 313E
+
+#  Unique Number: 
+
+#  Date Created:01/19/2021
+
+#  Date Last Modified:
+
+# Input: s1 and s2 are two strings that represent strands of DNA
+# Output: returns a sorted list of substrings that are the longest 
+#         common subsequence. The list is empty if there are no 
+#         common subsequences.
+
+import sys 
+
+
+def longest_subsequence (s1, s2):
+    y = 4
+    
+
+
+def main():
+
+  # read the data
+    
+    content = sys.stdin.readlines()
+    n = content[0]
+
+
+
+  # for each pair
+    for i in range(n-1):
+        
+    # call longest_subsequence
+
+    # write out result(s)
+
+	# insert blank line
+
+if __name__ == "__main__":
+  main()
